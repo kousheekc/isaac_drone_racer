91c99bcdb233e02e7c4524cc07c36d5747e2f6a6
kousheekc <kousheekc@gmail.com>
2025-07-23 23:37:21 +0200

    rsl rl and onnx inference

--- tasks/drone_racer/__init__.py
+++ tasks/drone_racer/__init__.py
@@ -17,15 +17,6 @@ gym.register(
     kwargs={
         "env_cfg_entry_point": f"{__name__}.drone_racer_env_cfg:DroneRacerEnvCfg",
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_cfg.yaml",
-    },
-)
-
-gym.register(
-    id="Isaac-Drone-Racer-Play-v0",
-    entry_point="isaaclab.envs:ManagerBasedRLEnv",
-    disable_env_checker=True,
-    kwargs={
-        "env_cfg_entry_point": f"{__name__}.drone_racer_env_cfg:DroneRacerEnvCfg_PLAY",
-        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_cfg:IsaacDroneRacerRunnerCfg",
     },
 )
