6a10a72efd87e92bdef2fc355b1add6440f4f5a4
kousheekc <kousheekc@gmail.com>
2025-07-29 00:01:07 +0200

    randomise inertia

--- tasks/drone_racer/drone_racer_env_cfg.py
+++ tasks/drone_racer/drone_racer_env_cfg.py
@@ -101,8 +101,18 @@ class EventCfg:
         func=mdp.randomize_rigid_body_mass,
         mode="reset",
         params={
-            "asset_cfg": SceneEntityCfg("robot", body_names=".*"),
+            "asset_cfg": SceneEntityCfg("robot", body_names="body*"),
             "mass_distribution_params": (0.8, 1.2),
+            "operation": "scale",
+        },
+    )
+
+    randomize_inertia = EventTerm(
+        func=mdp.randomize_rigid_body_inertia,
+        mode="reset",
+        params={
+            "asset_cfg": SceneEntityCfg("robot", body_names="body"),
+            "inertia_distribution_params": (0.8, 1.2),
             "operation": "scale",
         },
     )
